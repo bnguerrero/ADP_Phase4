1f11ea938fa4541a65c423be3ba162e31eef84ea
bnguerrero <bnguerrero@cpp.edu>
2024-08-22 20:55:12 +0000

    initial commit

---
+++ src/main/java/com/example/phase4Auth/Phase4AuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.phase4Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase4AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase4AuthApplication.class, args);
+	}
+
+}
